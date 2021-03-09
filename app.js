2d863d2c9db732f7bca11eead4e418d9a7338e6b
ingoo-code <web7722@kakao.com>
2021-03-09 17:18:44 +0900

    update Hello world text

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send('hello, world!'));
+app.get('/', (req,res) => res.send('hello, world!22'));
 app.listen(port,()=>console.log(`Example app listening on port ${port}!`));
